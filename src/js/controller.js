70bf50fa0218ae10bd28e8108249273cbf6e8544
Blessing U <blessing986@users.noreply.github.com>
2023-10-02 18:36:34 +0100

    chore: event handlers in MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,13 +6,7 @@ import 'regenerator-runtime/runtime'; // polyfilling async await
 
 const recipeContainer = document.querySelector('.recipe');
 
-// const timeout = function (s) {
-//   return new Promise(function (_, reject) {
-//     setTimeout(function () {
-//       reject(new Error(`Request took too long! Timeout after ${s} second`));
-//     }, s * 1000);
-//   });
-// };
+//
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -33,13 +27,12 @@ const controlRecipes = async () => {
     // 2. Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = () => {
+  recipeView.addHandlerRender(controlRecipes);
+};
 
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+init();
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import {Fraction} from 'fractional';
+import { Fraction } from 'fractional';
 console.log(Fraction);
 
 class RecipeView {
@@ -27,6 +27,10 @@ class RecipeView {
     this.#parentElement.innerHTML = '';
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   };
+
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+  }
 
   #generateMarkup() {
     return `
